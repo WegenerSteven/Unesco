7e2fc10fcf24938389ec6cea778f29aea69389b1
WegenerSteven <wegenersteven580@gmail.com>
2024-05-29 15:12:32 +0300

    initial commit

---
+++ samples/demo.js
@@ -0,0 +1,6 @@
+const loginForm = document.getElementById('login-form');
+const openLoginForm = document.getElementById('open-login-form');
+
+openLoginForm.addEventListener('click', () => {
+  loginForm.style.display = 'block';
+}); 
